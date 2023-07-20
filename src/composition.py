d46fb5ce6a69a1546bf856ae54652b03b2b66eef
Jonas Busk <jonasbusk@gmail.com>
2023-07-20 10:39:30 +0200

    Add composition

---
+++ src/composition.py
@@ -0,0 +1,194 @@
+"""Tools for working with compositions.
+
+Resources:
+    https://en.wikipedia.org/wiki/Compositional_data
+    https://link.springer.com/book/10.1007/978-3-319-96422-5
+    https://composition-stats.readthedocs.io/en/latest/
+"""
+
+import torch
+
+
+# Basic operations
+
+
+def closure(c: torch.Tensor, k=1.0) -> torch.Tensor:
+    """Closure operator for compositions.
+
+    Scale compositions c so that they sum to k.
+    """
+    c = torch.atleast_2d(c)
+    assert c.ndim == 2
+    c = c / c.sum(dim=1, keepdim=True)
+    return k * c
+
+
+def perturb(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    """Perturbation operator for compositions."""
+    assert x.shape == y.shape
+    x, y = closure(x), closure(y)
+    return closure(x * y)
+
+
+def perturb_inv(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    """Inverse perturbation operator for compositions."""
+    assert x.shape == y.shape
+    x, y = closure(x), closure(y)
+    return closure(x / y)
+
+
+def power(x: torch.Tensor, a: float) -> torch.Tensor:
+    """Power operator for compositions."""
+    x = closure(x)
+    return closure(x ** a)
+
+
+# Aitchison geometry
+
+
+def inner(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    """Aitchison inner product for compositions."""
+    x, y = closure(x), closure(y)
+    a, b = clr(x), clr(y)
+    return a @ b.T
+
+
+def norm(x: torch.Tensor) -> torch.Tensor:
+    """Aitchison norm for compositions."""
+    return torch.sqrt(inner(x, x))
+
+
+def dist(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    """Aitchison distance for compositions."""
+    x, y = closure(x), closure(y)
+    a, b = clr(x), clr(y)
+    return torch.linalg.norm(a - b, dim=1)
+
+
+# Transformations
+
+
+def clr(x: torch.Tensor) -> torch.Tensor:
+    """Centered log-ratio transformation."""
+    lx = torch.log(closure(x))
+    lgm = lx.mean(dim=1, keepdim=True)  # log geometric mean
+    return lx - lgm
+
+
+def clr_inv(x: torch.Tensor) -> torch.Tensor:
+    """Inverse centered log-ratio transformation.
+
+    Transform clr representation back to the original composition up to a scaling factor.
+    """
+    return closure(torch.exp(x))
+
+
+def ilr(x: torch.Tensor) -> torch.Tensor:
+    """Isometric log-ratio transformation."""
+    basis = _pivot_basis(x.shape[-1])
+    return inner(x, basis)
+
+
+def ilr_inv(x: torch.Tensor) -> torch.Tensor:
+    """Inverse isometric log-ratio transformation.
+
+    Transform ilr representation back to the original composition up to a scaling factor.
+    """
+    basis = _pivot_basis(x.shape[-1] + 1)
+    return clr_inv(x @ basis)  # TODO: basis.T?
+
+
+def _pivot_basis(D: int) -> torch.Tensor:
+    """Orthonormal basis for the ilr coordinates.
+
+    Args:
+        D: Dimension of the Aitchison simplex."""
+    # basis = np.zeros((n, n-1))
+    # for j in range(n-1):
+    #     i = j + 1
+    #     e = np.array([(1/i)]*i + [-1] +
+    #                  [0]*(n-i-1))*np.sqrt(i/(i+1))
+    #     basis[:, j] = e
+    # return basis.T
+    raise NotImplementedError
+
+
+# Sampling
+
+
+def sample_compositions(num_features: int, num_samples: int = 1) -> torch.Tensor:
+    """Sample compositions.
+
+    The sample space is the (num_features-1)-standard simplex.
+    The values of each sampled composition are between 0 and 1 and all values sum to 1.
+
+    Args:
+        num_features: Number of features.
+        num_samples: Number of samples.
+    Returns:
+        Tensor of sampled compositions (num_samples, num_features).
+    """
+    x = torch.rand(num_samples, num_features + 1)
+    x[:, 0] = 0
+    x[:, -1] = 1
+    x, _ = torch.sort(x)  # cumulative compositions
+    x = x[:, 1:] - x[:, :-1]  # compositions
+    return x
+
+
+def sample_compositions_with_constraints(
+        lower: list[float], upper: list[float], tolerance: list[float],
+        num_samples: int = 1, batch_size: int = 100) -> torch.Tensor:
+    """Sample compositions with the given constraints.
+
+    The sample space is the (num_features-1)-standard simplex with the given constraints.
+    The values of each sampled composition are between lower and higher and all values sum to 1.
+
+    Warning:
+    While tolerance and lower bounds come for free, sampling is inefficient for tight upper bounds.
+    Especially if the sum of the upper bounds is close to 1.0 and the number of features is large.
+
+    Args:
+        lower: Lower bounds (num_features,).
+        upper: Upper bounds (num_features,).
+        tolerance: Values below tolerance are rounded to zero (num_features,).
+        num_samples: Number of samples.
+        batch_size: Batch size for sampling.
+    Returns:
+        Tensor of sampled compositions (num_samples, num_features).
+    """
+    num_features = len(lower)
+    lower, upper, tolerance = torch.tensor(lower), torch.tensor(upper), torch.tensor(tolerance)
+    assert num_features >= 2, "At least two features are required."
+    assert len(lower) == len(upper) == len(tolerance), \
+        "Constraints must all have length (num_features,)."
+    assert min(lower) >= 0 and max(lower) <= 1, "Lower bounds must be between 0 and 1."
+    assert min(upper) >= 0 and max(upper) <= 1, "Upper bounds must be between 0 and 1."
+    assert min(tolerance) >= 0 and max(tolerance) <= 1.0 / num_features, \
+        "Tolerance must be between 0 and 1/num_features"
+    assert ((lower == 0) | (lower >= tolerance)).all(), \
+        f"Lower bounds must be zero or greater than tolerance {lower}."
+    assert all(upper - lower >= 0.1), "The sample space is too constrained."
+    assert sum(lower) <= 1.0, "The sample space is too constrained."
+    assert sum(upper) >= 1.1, "The sample space is too constrained."
+    # Sample compositions iteratively
+    samples = []
+    while sum([len(x) for x in samples]) < num_samples:
+        # Sample unconstrained batch of candidate compositions
+        candidates = sample_compositions(num_features, batch_size)
+        # Apply tolerance
+        if tolerance.sum() > 0:
+            # Scale tolerance since lower bounds are added below
+            mask = candidates < (tolerance - lower) / (1.0 - lower.sum())
+            candidates[mask] = 0  # Set values below tolerance to zero
+            candidates = candidates / candidates.sum(dim=1, keepdim=True)  # renormalize
+        # Apply lower bounds
+        candidates = lower + (1.0 - lower.sum()) * candidates
+        # Apply upper bounds
+        if upper.sum() < num_features:
+            mask = (candidates <= upper).all(dim=1)
+            candidates = candidates[mask]
+        # Append to samples
+        samples.append(candidates)
+    samples = torch.cat(samples)
+    return samples[:num_samples]
---
+++ tests/test_composition.py
@@ -0,0 +1,210 @@
+import unittest
+
+import torch
+
+from src import composition
+
+
+class TestCompositionBasicOperators(unittest.TestCase):
+
+    def test_closure(self):
+        x = torch.tensor([0.1, 0.2, 0.3, 0.4])
+        self.assertTrue(torch.allclose(composition.closure(x), x))
+        x = torch.tensor([1.0, 2.0, 3.0, 4.0])
+        self.assertTrue(torch.allclose(composition.closure(x, 10.0), x))
+
+    def test_perturb(self):
+        x = torch.tensor([0.1, 0.2, 0.3, 0.4])
+        y = torch.tensor([1./6, 1./3, 1./6, 1./3])
+        z = torch.tensor([0.0625, 0.25, 0.1875, 0.5])
+        self.assertTrue(torch.allclose(composition.perturb(x, y), z))
+        self.assertTrue(torch.allclose(composition.perturb(x, torch.ones_like(x)), x))
+
+    def test_perturb_inv(self):
+        x = torch.tensor([[0.1, 0.2, 0.3, 0.4]])
+        y = torch.tensor([[1./6, 1./3, 1./6, 1./3]])
+        z = torch.tensor([0.14285714, 0.14285714, 0.42857143, 0.28571429])
+        n = composition.closure(torch.ones_like(x))
+        self.assertTrue(torch.allclose(composition.perturb_inv(x, y), z))
+        self.assertTrue(torch.allclose(composition.perturb_inv(composition.perturb(x, y), y), x))
+        self.assertTrue(torch.allclose(composition.perturb_inv(composition.perturb(x, y), x), y))
+        self.assertTrue(torch.allclose(composition.perturb_inv(x, x), n))
+
+    def test_power(self):
+        x = torch.tensor([.1, .3, .4, .2])
+        y = torch.tensor([0.23059566, 0.25737316, 0.26488486, 0.24714631])
+        n = composition.closure(torch.ones_like(x))
+        self.assertTrue(torch.allclose(composition.power(x, 0.1), y))
+        self.assertTrue(torch.allclose(composition.power(x, 1.0), x))
+        self.assertTrue(torch.allclose(composition.power(x, 0.0), n))
+
+
+class TestCompositionAitchisonGeometry(unittest.TestCase):
+
+    def test_inner(self):
+        x = torch.tensor([[0.1, 0.2, 0.3, 0.4]])
+        y = torch.tensor([[0.1, 0.1, 0.4, 0.4]])
+        z = torch.tensor([[0.4, 0.4, 0.1, 0.1]])
+        n = composition.closure(torch.ones_like(x))
+        # The neutral composition is the zero vector
+        self.assertEqual(composition.inner(n, n).item(), 0.0)
+        self.assertEqual(composition.inner(x, n).item(), 0.0)
+        # Points in the same direction
+        self.assertGreater(composition.inner(x, y).item(), 0.0)
+        # Points in opposite directions
+        self.assertLess(composition.inner(x, z).item(), 0.0)
+
+    def test_norm(self):
+        x = torch.tensor([[0.1, 0.2, 0.3, 0.4]])
+        y = torch.tensor([[0.1, 0.7, 0.1, 0.1]])
+        n = composition.closure(torch.ones_like(x))
+        self.assertGreater(composition.norm(x).item(), 0.0)
+        self.assertEqual(composition.norm(n).item(), 0.0)
+        self.assertGreater(composition.norm(y).item(), composition.norm(x).item())
+
+    def test_dist(self):
+        x = torch.tensor([
+            [0.1, 0.2, 0.3, 0.4],
+            [0.1, 0.3, 0.3, 0.3],
+        ])
+        y = torch.tensor([
+            [0.1, 0.3, 0.3, 0.3],
+            [0.1, 0.2, 0.3, 0.4],
+        ])
+        d = composition.dist(x, x)
+        self.assertTrue(torch.allclose(d, torch.zeros_like(d)))
+        d = composition.dist(x, y)
+        self.assertTrue(torch.all(d == d[0]))
+
+
+class TestCompositionTransformations(unittest.TestCase):
+
+    def test_clr(self):
+        x = torch.tensor([[0.1, 0.2, 0.3, 0.4]])
+        y = torch.tensor([[0.1, 0.3, 0.3, 0.3]])
+        n = composition.closure(torch.ones_like(x))
+        c = 0.987654321
+        self.assertAlmostEqual(composition.clr(x).sum().item(), 0.0, 5)
+        self.assertTrue(torch.allclose(composition.clr(n), torch.zeros_like(n)))
+        self.assertTrue(torch.allclose(composition.clr_inv(composition.clr(x)), x))
+        self.assertTrue(torch.allclose(composition.clr_inv(composition.clr(y)), y))
+        self.assertTrue(composition.clr(x).shape[-1] == x.shape[-1])
+        # linearity
+        self.assertTrue(torch.allclose(
+            composition.clr(composition.perturb(x, y)),
+            composition.clr(x) + composition.clr(y)
+        ))
+        self.assertTrue(torch.allclose(
+            composition.clr(composition.power(x, c)),
+            composition.clr(x) * c
+        ))
+        # inner product
+        self.assertTrue(torch.allclose(
+            composition.inner(x, y),
+            composition.clr(x) @ composition.clr(y).T
+        ))
+        self.assertTrue(torch.allclose(
+            composition.norm(x),
+            torch.linalg.norm(composition.clr(x))
+        ))
+        # distance
+        self.assertTrue(torch.allclose(
+            composition.dist(x, y),
+            torch.linalg.norm(composition.clr(x) - composition.clr(y), dim=1)
+        ))
+
+    def test_ilr(self):
+        x = torch.tensor([[0.1, 0.2, 0.3, 0.4]])
+        y = torch.tensor([[0.1, 0.3, 0.3, 0.3]])
+        # n = composition.closure(torch.ones_like(x))
+        c = 0.987654321
+        # self.assertAlmostEqual(composition.clr(x).sum().item(), 0.0, 5)
+        # self.assertTrue(torch.allclose(composition.clr(n), torch.zeros_like(n)))
+        self.skipTest("TODO: Finish implementation.")
+        self.assertTrue(torch.allclose(composition.ilr_inv(composition.ilr(x)), x))
+        self.assertTrue(torch.allclose(composition.ilr_inv(composition.ilr(y)), y))
+        self.assertTrue(composition.ilr(x).shape[-1] == x.shape[-1] - 1)
+        # linearity
+        self.assertTrue(torch.allclose(
+            composition.ilr(composition.perturb(x, y)),
+            composition.ilr(x) + composition.ilr(y)
+        ))
+        self.assertTrue(torch.allclose(
+            composition.ilr(composition.power(x, c)),
+            composition.ilr(x) * c
+        ))
+        # inner product
+        self.assertTrue(torch.allclose(
+            composition.inner(x, y),
+            composition.ilr(x) @ composition.ilr(y).T
+        ))
+        self.assertTrue(torch.allclose(
+            composition.norm(x),
+            torch.linalg.norm(composition.ilr(x))
+        ))
+        # distance
+        self.assertTrue(torch.allclose(
+            composition.dist(x, y),
+            torch.linalg.norm(composition.ilr(x) - composition.ilr(y), dim=1)
+        ))
+
+
+class TestCompositionSampling(unittest.TestCase):
+
+    def test_sample_compositions(self):
+        """Test sample compositions."""
+        num_features, num_samples = 10, 1000
+        x = composition.sample_compositions(num_features, num_samples)
+        self.assertEqual(x.shape, (num_samples, num_features))
+        self.assertTrue((x >= 0).all())
+        self.assertTrue((x <= 1).all())
+        self.assertTrue((x.sum(dim=1) == 1).all())
+        self.assertTrue((x.min(0)[0] < 0.05).all())
+
+    def test_sample_compositions_with_constraints_negative(self):
+        """Test compositions are not sampled outside the given constraints."""
+        num_features, num_samples = 10, 1000
+        # Constraints
+        tolerance = 0.01
+        lower = torch.rand(num_features) * (0.5 / num_features)
+        lower[lower < tolerance] = 0
+        lower[0] = 0.1
+        upper = torch.ones(num_features) - torch.rand(num_features) * (0.5 / num_features)
+        upper[1] = 0.5
+        tolerance = torch.ones(num_features) * tolerance
+        # Sample compositions
+        x = composition.sample_compositions_with_constraints(lower, upper, tolerance, num_samples)
+        # Check
+        self.assertEqual(x.shape, (num_samples, num_features))
+        self.assertTrue((x >= 0).all())
+        self.assertTrue((x <= 1).all())
+        self.assertTrue((torch.abs(x.sum(dim=1) - 1.0) < 1e-6).all())
+        self.assertTrue(((x == 0) | (x >= tolerance)).all())
+        self.assertTrue((x >= lower).all())
+        self.assertTrue((x <= upper).all())
+
+    def test_sample_compositions_with_constraints_positive(self):
+        """Test the sampled compositions cover the volume inside the given constraints."""
+        num_features, num_samples = 2, 1000
+        # Sample without constraints and check that min and max values are close to 0 and 1
+        zeros = torch.zeros(num_features)
+        ones = torch.ones(num_features)
+        x = composition.sample_compositions_with_constraints(zeros, ones, zeros, num_samples)
+        self.assertEqual(x.shape, (num_samples, num_features))
+        self.assertTrue((torch.abs(x.sum(dim=1) - 1.0) < 1e-6).all())
+        self.assertTrue((x.min(0)[0] < 0.05).all())
+        self.assertTrue((x.max(0)[0] > 0.95).all())
+        # Sample with lower and upper bounds and check that values are close to the bounds
+        lower = torch.tensor([0.05, 0.05])
+        upper = torch.tensor([0.95, 0.95])
+        x = composition.sample_compositions_with_constraints(lower, upper, zeros, num_samples)
+        self.assertEqual(x.shape, (num_samples, num_features))
+        self.assertTrue((torch.abs(x.sum(dim=1) - 1.0) < 1e-6).all())
+        self.assertTrue((x.min(0)[0] < (lower + 0.05)).all())
+        self.assertTrue((x.max(0)[0] > (upper - 0.05)).all())
+        # Sample with tolerance and check that values are rounded to exactly 0 and 1
+        x = composition.sample_compositions_with_constraints(zeros, ones, lower, num_samples)
+        self.assertEqual(x.shape, (num_samples, num_features))
+        self.assertTrue((torch.abs(x.sum(dim=1) - 1.0) < 1e-6).all())
+        self.assertTrue((x.min(0)[0] == 0.0).all())
+        self.assertTrue((x.max(0)[0] == 1.0).all())
