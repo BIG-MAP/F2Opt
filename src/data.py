402aef6a2b48440597a47f7f87d661dd2d5e629e
Jonas Busk <jonasbusk@gmail.com>
2023-08-01 13:54:00 +0200

    Update processing of limitations

--- src/data.py
+++ src/data.py
@@ -1,15 +1,132 @@
 """Data processing.
 
-Parse data from the broker to a format that can be used by the optimiser.
-Prepare optimiser results in a format that can be send to the broker.
-Use schemas to validate data formats.
+- Parse data from the broker to a format that can be used by the optimiser.
+- Prepare optimiser results in a format that can be send to the broker.
+- Use schemas to validate data formats.
 """
 
-from collections import defaultdict
 from datetime import datetime
+
+import pandas as pd
 
 from . import schemas
 from .composition import sample_compositions_with_constraints
+
+
+# Constraints
+
+
+def get_constraints_from_method_limitations(method_limitations):
+    """Get constraints for each method.
+
+    Args:
+        method_limitations (list): A list of methods with limitations.
+    Returns:
+        Constraints dict: {method: constraints}.
+    """
+    constraints = {}
+    for method in method_limitations:
+        constraints[method["method"]] = get_constraints_from_method(method)
+    return constraints
+
+
+def get_constraints_from_method(method):
+    """Get constraints from method limitations.
+
+    Args:
+        method (dict): A method with limitations.
+    Returns:
+        List of method constraints dicts.
+    """
+    # Extract limitation from method
+    assert len(method["limitations"]) == 1
+    limitations = method["limitations"][0]
+    # Extract constraints for each formulation limitations
+    constraints = []
+    for formulation in limitations["formulation"]:
+        constraints.append({
+            "quantity": method["quantity"],
+            "method": method["method"],
+            "formulation": get_constraints_from_formulation(formulation),
+        })
+    return constraints
+
+
+def get_constraints_from_formulation(formulation):
+    """Get formulation constraints from formulation limitations.
+
+    Args:
+        formulation (list of dict): A list of chemicals with limitations.
+    Returns:
+        Formulation constraints dict.
+    """
+    chemicals, lower, upper, tolerance = [], [], [], []
+    for chemical in formulation:
+        chemicals.append(chemical["chemical"])
+        l, u, t = get_constraints_from_fraction(chemical["fraction"])
+        lower.append(l)
+        upper.append(u)
+        tolerance.append(t)
+    assert len(formulation) == len(chemicals) == len(lower) == len(upper) == len(tolerance)
+    constraints = {
+        "chemicals": chemicals, "lower": lower, "upper": upper, "tolerance": tolerance
+    }
+    return constraints
+
+
+def get_constraints_from_fraction(fraction):
+    """Get fraction constraints from fraction limitations.
+
+    Returns lower bound, upper bound and tolerance.
+    - Values below tolerance should be rounded to zero.
+    - Tolerance is zero by default.
+    - If tolerance is defined, lower bound should be zero.
+
+    Example input: [{"min": 0, "max": 0}, {"min": 0.1, "max": 1.0}]
+
+    Supported input cases where 0 is the constant zero, a is min and b is max:
+    - [(a, b)]:         Lower and upper bounds. Tolerance defaults zero.
+    - [(0, 0), (a, b)]: Lower is zero and tolerance is a.
+    - [(a, b), (0, 0)]: Lower is zero and tolerance is a.
+    - [0, (a, b)]:      Lower is zero and tolerance is a.
+    - [(a, b), 0]:      Lower is zero and tolerance is a.
+
+    Args:
+        fraction (list): Fraction limitations.
+    Returns:
+        Fraction constraints tuple of numbers(lower, upper, tolerance).
+    """
+    assert len(fraction) == 1 or len(fraction) == 2
+    lower, upper, tolerance = None, None, 0.0
+    for x in fraction:
+        if isinstance(x, (int, float)):  # if it is a number, it is the lower bound
+            if lower is not None:  # this is the second element in the list
+                assert lower >= x, fraction
+                assert x == 0.0, fraction
+                tolerance = lower
+            lower = x
+        elif isinstance(x, dict):
+            if x["min"] == x["max"]:  # if min and max are the same, it is the lower bound
+                if lower is not None:  # this is the second element in the list
+                    assert lower >= x["min"], fraction
+                    assert x["min"] == 0.0, fraction
+                    tolerance = lower
+                lower = x["min"]
+            else:  # min and max are different
+                assert x["min"] < x["max"]
+                if lower is None:
+                    lower = x["min"]
+                else:
+                    assert lower == 0.0, fraction
+                    tolerance = x["min"]
+                upper = x["max"]
+    assert lower is not None, (lower, upper, tolerance, fraction)
+    assert upper is not None, (lower, upper, tolerance, fraction)
+    assert tolerance == 0.0 or tolerance > lower, (lower, upper, tolerance, fraction)
+    return lower, upper, tolerance
+
+
+# Results
 
 
 def get_dataframe_from_results(config, results):
@@ -22,52 +139,94 @@ def get_row_from_result(result):
     raise NotImplementedError
 
 
-def get_constraints_from_limitations(config, limitations):
-    """Get constraints for each target in the config."""
-    # TODO: Update to current format of limitations
-    constraints = defaultdict(dict)  # {method: constraints}
-    for limitation in limitations:
-        formulation = limitation["limitations"]["formulation"]
-        chemicals = [x["chemical"] for x in formulation]  # list of available chemicals
-        lower = [x["fraction"]["min"] for x in formulation]  # list of lower bounds
-        upper = [x["fraction"]["max"] for x in formulation]  # list of upper bounds
-        tolerance = [0.0 for x in formulation]  # list of tolerances
-        assert len(formulation) == len(chemicals) == len(lower) == len(upper) == len(tolerance)
-        # constraints[target["name"]]["formulation"] = {
-        constraints[limitation["method"]]["formulation"] = {
-            "chemicals": chemicals, "lower": lower, "upper": upper, "tolerance": tolerance
-        }
-    assert len(constraints) == len(config["targets"])
-    assert all(target["method"] in constraints for target in config["targets"])
-    return constraints
+# Requests
+
+
+def get_candidates_from_method_constraints(constraints_list, num_samples=1):
+    """Get dataframe of candidate compositions from list of constraints.
+
+    Args:
+        constraints_list (list): A list of constraints for a single method.
+        num_samples (int): Number of samples per set of constraints.
+    Returns:
+        Dataframe of candidates with columns for each chemical.
+    """
+    assert all("quantity" in c for c in constraints_list), constraints_list
+    assert all("method" in c for c in constraints_list), constraints_list
+    assert all("formulation" in c for c in constraints_list), constraints_list
+    assert len(set(c["quantity"] for c in constraints_list)) == 1, constraints_list
+    assert len(set(c["method"] for c in constraints_list)) == 1, constraints_list
+    # Sample candidate compositions for each set of formulation constraints
+    dfs = []  # List of candidate dataframes
+    for constraints in constraints_list:
+        formulation = constraints["formulation"]
+        compositions = sample_compositions_with_constraints(
+            lower=formulation["lower"],
+            upper=formulation["upper"],
+            tolerance=formulation["tolerance"],
+            num_samples=num_samples,
+        )
+        columns = [chem["SMILES"] for chem in formulation["chemicals"]]
+        dfs.append(pd.DataFrame(compositions, columns=columns))
+    # Stack candidate samples and fill missing values with zeros
+    candidates_df = pd.concat(dfs, axis=0, ignore_index=True).fillna(0)
+    return candidates_df
 
 
 def get_best_candidates(config, df, constraints):
-    """Get best candidates for all targets and quantities."""
-    # TODO: Implement data-driven optimisation instead of random sampling
-    candidates = {}
+    """Get best candidates for all target methods and quantities.
+
+    Args:
+        config (dict): Optimiser configuration.
+        df (pandas.DataFrame): Dataframe of observed data.
+        constraints (dict): Constraints for each method.
+    Returns:
+        Dict of best candidates for each target method.
+    """
+    # TODO: Check df has the correct columns (chemicals and quantities)
+    result = {}  # {method: candidate}
     for target in config["targets"]:
-        method = target["method"]
-        chemicals = constraints[method]["formulation"]["chemicals"]
-        # Generate random candidates within constraints
-        fractions = sample_compositions_with_constraints(
-            lower=constraints[method]["formulation"]["lower"],
-            upper=constraints[method]["formulation"]["upper"],
-            tolerance=constraints[method]["formulation"]["tolerance"],
-        )[0]
+        # method_name = method_config["method"]
+        method_constraints = constraints[target["method"]]
+        assert all(cons["method"] == target["method"] for cons in method_constraints)
+        # Create dict of all chemicals included in the method constraints
+        all_chemicals = {}  # {smiles: chemical}
+        for cons in method_constraints:
+            for chemical in cons["formulation"]["chemicals"]:
+                all_chemicals[chemical["SMILES"]] = chemical
+        # Get dataframe of candidates for the method
+        # TODO: Use config to set number of samples
+        candidates_df = get_candidates_from_method_constraints(method_constraints, num_samples=1)
+        # Get random candidate for the method
+        # TODO: Implement data-driven optimisation instead of random sampling
+        candidate_row = candidates_df.sample(n=1)  # Sample row
+        # Extract chemicals and fractions from row
+        chemicals = [all_chemicals[smiles] for smiles in candidate_row.columns]
+        fractions = candidate_row.values[0].tolist()
         assert len(chemicals) == len(fractions)
+        # Prepare candidate
         candidate = {
+            # "quantity": target["quantity"],
+            # "method": target["method"],
             "target": target,
+            # "method_constraints": method_constraints,
             "chemicals": chemicals,
             "fractions": fractions,
             # "objectives": None,  # TODO: Include the predicted value?
         }
-        candidates[target["name"]] = candidate
-    return candidates
+        result[target["method"]] = candidate
+    return result
 
 
 def get_requests_from_candidate(config, candidate):
-    """Prepare request from candidate."""
+    """Prepare request from candidate.
+
+    Args:
+        config (dict): Optimiser configuration.
+        candidate (dict): Candidate.
+    Returns:
+        List of requests.
+    """
     target = candidate["target"]
     # Formulation: List[FINALES2_schemas.classes_common.FormulationComponent]
     formulation = []
--- tests/test_data.py
+++ tests/test_data.py
@@ -10,46 +10,39 @@ class TestData(unittest.TestCase):
     #     results = []
     #     data.get_dataframe_from_results(config, results)
 
-    def test_get_constraints_from_limitations(self):
-        config = {
-            "targets": [{
-                "name": "test_target",
-                "quantities": {"test_quantity": "test_quantity"},
-                "method": "test_method",
-                "defaults": {},
-                "max_queue_size": 1
-            }]
-        }
+    def test_get_constraints_from_method_limitations(self):
         limitations = [
             {
                 "quantity": "test_quantity",
                 "method": "test_method",
-                "limitations": {
-                    "formulation": [
-                        {
-                            "chemical": {
-                                "SMILES": "test_smiles_1",
-                                "InChIKey": "test_inchi_1"
+                "limitations": [
+                    {
+                        "formulation": [[
+                            {
+                                "chemical": {"SMILES": "test_smiles_1", "InChIKey": "test_inchi_1"},
+                                # "fraction": [{"min": 0.0, "max": 1.0}],
+                                "fraction": [{"min": 0.1, "max": 1.0}, {"min": 0.0, "max": 0.0}],
+                                "fraction_type": "molPerMol"
                             },
-                            "fraction": {"min": 0.1, "max": 0.9},
-                            "fraction_type": ["molPerMol"]
-                        },
-                        {
-                            "chemical": {
-                                "SMILES": "test_smiles_2",
-                                "InChIKey": "test_inchi_2"
+                            {
+                                "chemical": {"SMILES": "test_smiles_2", "InChIKey": "test_inchi_2"},
+                                # "fraction": [{"min": 0.0, "max": 1.0}],
+                                "fraction": [{"min": 0.1, "max": 1.0}, 0.0],
+                                "fraction_type": "molPerMol"
                             },
-                            "fraction": {"min": 0.1, "max": 0.9},
-                            "fraction_type": ["molPerMol"]
-                        },
-                    ]
-                }
+                        ]],
+                        "temperature": [{"min": 243, "max": 333}]
+                    }
+                ]
             }
         ]
-        constraints = data.get_constraints_from_limitations(config, limitations)
+        constraints = data.get_constraints_from_method_limitations(limitations)
         self.assertIn("test_method", constraints)
-        self.assertIn("formulation", constraints["test_method"])
-        formula_constraints = constraints["test_method"]["formulation"]
+        self.assertEqual(len(constraints["test_method"]), 1)
+        self.assertEqual(constraints["test_method"][0]["method"], "test_method")
+        self.assertEqual(constraints["test_method"][0]["quantity"], "test_quantity")
+        self.assertIn("formulation", constraints["test_method"][0])
+        formula_constraints = constraints["test_method"][0]["formulation"]
         self.assertIn("chemicals", formula_constraints)
         self.assertIn("lower", formula_constraints)
         self.assertIn("upper", formula_constraints)
@@ -59,40 +52,185 @@ class TestData(unittest.TestCase):
         for lower, upper in zip(formula_constraints["lower"], formula_constraints["upper"]):
             self.assertGreater(upper, lower)
 
-    def test_get_best_candidates(self):
-        config = {
-            "name": "opt",
-            "targets": [{
-                "name": "test_target",
-                "quantities": {"test_quantity": "test_quantity"},
-                "method": "test_method",
-                "defaults": {"temperature": 300},
-                "max_queue_size": 1,
-            }]
-        }
-        df = None
-        chemicals = [
+    def test_get_constraints_from_formulation(self):
+        formulation = [
             {
-                "SMILES": "test_smiles_1",
-                "InChIKey": "test_inchi_1",
+                "chemical": {"SMILES": "test_smiles_1", "InChIKey": "test_inchi_1"},
+                # "fraction": [{"min": 0.0, "max": 1.0}],
+                "fraction": [{"min": 0.1, "max": 1.0}, {"min": 0.0, "max": 0.0}],
+                "fraction_type": "molPerMol"
             },
             {
-                "SMILES": "test_smiles_2",
-                "InChIKey": "test_inchi_2",
+                "chemical": {"SMILES": "test_smiles_2", "InChIKey": "test_inchi_2"},
+                # "fraction": [{"min": 0.0, "max": 1.0}],
+                "fraction": [{"min": 0.1, "max": 1.0}, 0.0],
+                "fraction_type": "molPerMol"
             },
         ]
-        constraints = {"test_method": {"formulation": {
-            "chemicals": chemicals,
-            "lower": [0.1, 0.1],
-            "upper": [0.9, 0.9],
-            "tolerance": [0.0, 0.0],
-        }}}
+        formula_constraints = data.get_constraints_from_formulation(formulation)
+        self.assertIn("chemicals", formula_constraints)
+        self.assertIn("lower", formula_constraints)
+        self.assertIn("upper", formula_constraints)
+        self.assertIn("tolerance", formula_constraints)
+        lengths = [len(formula_constraints[k]) for k in formula_constraints.keys()]
+        self.assertEqual(len(set(lengths)), 1)  # all lists have the same length
+        for lower, upper in zip(formula_constraints["lower"], formula_constraints["upper"]):
+            self.assertGreater(upper, lower)
+
+    def test_get_constraints_from_fraction(self):
+        fraction = [{"min": 0.0, "max": 1.0}]
+        lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+        self.assertEqual(lower, 0.0)
+        self.assertEqual(upper, 1.0)
+        self.assertEqual(tolerance, 0.0)
+
+        fraction = [{"min": 0.1, "max": 0.9}]
+        lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+        self.assertEqual(lower, 0.1)
+        self.assertEqual(upper, 0.9)
+        self.assertEqual(tolerance, 0.0)
+
+        fraction = [{"min": 0.0, "max": 0.0}, {"min": 0.0, "max": 1.0}]
+        lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+        self.assertEqual(lower, 0.0)
+        self.assertEqual(upper, 1.0)
+        self.assertEqual(tolerance, 0.0)
+
+        fraction = [{"min": 0.0, "max": 0.0}, {"min": 0.1, "max": 0.9}]
+        lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+        self.assertEqual(lower, 0.0)
+        self.assertEqual(upper, 0.9)
+        self.assertEqual(tolerance, 0.1)
+
+        fraction = [{"min": 0.0, "max": 1.0}, {"min": 0.0, "max": 0.0}]
+        lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+        self.assertEqual(lower, 0.0)
+        self.assertEqual(upper, 1.0)
+        self.assertEqual(tolerance, 0.0)
+
+        fraction = [{"min": 0.1, "max": 0.9}, {"min": 0.0, "max": 0.0}]
+        lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+        self.assertEqual(lower, 0.0)
+        self.assertEqual(upper, 0.9)
+        self.assertEqual(tolerance, 0.1)
+
+        fraction = [0.0, {"min": 0.1, "max": 0.9}]
+        lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+        self.assertEqual(lower, 0.0)
+        self.assertEqual(upper, 0.9)
+        self.assertEqual(tolerance, 0.1)
+
+        fraction = [{"min": 0.0, "max": 1.0}, 0.0]
+        lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+        self.assertEqual(lower, 0.0)
+        self.assertEqual(upper, 1.0)
+        self.assertEqual(tolerance, 0.0)
+
+        fraction = [{"min": 0.1, "max": 0.9}, 0.0]
+        lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+        self.assertEqual(lower, 0.0)
+        self.assertEqual(upper, 0.9)
+        self.assertEqual(tolerance, 0.1)
+
+        # should fail
+        # fraction = [{"min": 0.1, "max": 0.9}, {"min": 0.1, "max": 0.9}]
+        # lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+
+        # should fail
+        # fraction = [{"min": 0.1, "max": 0.1}, {"min": 0.2, "max": 0.9}]
+        # lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+
+        # should fail
+        # fraction = [{"min": 0.2, "max": 0.9}, {"min": 0.1, "max": 0.1}]
+        # lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+
+        # should fail
+        # fraction = [{"min": 0.1, "max": 0.7}, {"min": 0.3, "max": 0.9}]
+        # lower, upper, tolerance = data.get_constraints_from_fraction(fraction)
+
+    def test_get_candidates_from_method_constraints(self):
+        constraints = [
+            {
+                "quantity": "test_quantity",
+                "method": "test_method",
+                "formulation": {
+                    "chemicals": [
+                        {"SMILES": "test_smiles_1", "InChIKey": "test_inchi_1"},
+                        {"SMILES": "test_smiles_2", "InChIKey": "test_inchi_2"}
+                    ],
+                    "lower": [0.1, 0.1],
+                    "upper": [0.9, 0.9],
+                    "tolerance": [0.0, 0.0],
+                }
+            },
+            {
+                "quantity": "test_quantity",
+                "method": "test_method",
+                "formulation": {
+                    "chemicals": [
+                        {"SMILES": "test_smiles_2", "InChIKey": "test_inchi_2"},
+                        {"SMILES": "test_smiles_3", "InChIKey": "test_inchi_3"}
+                    ],
+                    "lower": [0.0, 0.0],
+                    "upper": [1.0, 1.0],
+                    "tolerance": [0.1, 0.1]
+                }
+            },
+        ]
+        num_samples = 1
+        candidates = data.get_candidates_from_method_constraints(constraints, num_samples)
+        self.assertEqual(len(candidates.columns), 3)
+        self.assertEqual(len(candidates), len(constraints) * num_samples)
+        self.assertIn("test_smiles_1", candidates.columns)
+        self.assertIn("test_smiles_2", candidates.columns)
+        self.assertIn("test_smiles_3", candidates.columns)
+
+    def test_get_best_candidates(self):
+        config = {
+            "name": "test_optimiser",
+            "targets": [{
+                "name": "test_target",
+                "quantities": {"test_quantity": "test_quantity"},
+                "method": "test_method",
+                "defaults": {"temperature": 300},
+                "max_queue_size": 1,
+            }]
+        }
+        constraints = {"test_method": [
+            {
+                "quantity": "test_quantity",
+                "method": "test_method",
+                "formulation": {
+                    "chemicals": [
+                        {"SMILES": "test_smiles_1", "InChIKey": "test_inchi_1"},
+                        {"SMILES": "test_smiles_2", "InChIKey": "test_inchi_2"},
+                    ],
+                    "lower": [0.1, 0.1],
+                    "upper": [0.9, 0.9],
+                    "tolerance": [0.0, 0.0],
+                }
+            },
+            {
+                "quantity": "test_quantity",
+                "method": "test_method",
+                "formulation": {
+                    "chemicals": [
+                        {"SMILES": "test_smiles_2", "InChIKey": "test_inchi_2"},
+                        {"SMILES": "test_smiles_3", "InChIKey": "test_inchi_3"},
+                    ],
+                    "lower": [0.0, 0.0],
+                    "upper": [1.0, 1.0],
+                    "tolerance": [0.1, 0.1]
+                }
+            }
+        ]}
+        df = None  # TODO: Add dataframe of observed data for data based approach
         candidates = data.get_best_candidates(config, df, constraints)
-        self.assertIn("test_target", candidates)
-        candidate = candidates["test_target"]
+        self.assertIn("test_method", candidates.keys())
+        candidate = candidates["test_method"]
         self.assertEqual(candidate["target"]["name"], "test_target")
 
-    def test_prepare_request(self):
+    def test_get_requests_from_candidate(self):
         config = {
             "name": "opt",
             "targets": [{
@@ -104,20 +242,62 @@ class TestData(unittest.TestCase):
             }]
         }
         chemicals = [
-            {
-                "SMILES": "test_smiles_1",
-                "InChIKey": "test_inchi_1",
-            },
-            {
-                "SMILES": "test_smiles_2",
-                "InChIKey": "test_inchi_2",
-            },
+            {"SMILES": "test_smiles_1", "InChIKey": "test_inchi_1"},
+            {"SMILES": "test_smiles_2", "InChIKey": "test_inchi_2"},
         ]
         candidate = {
             "target": config["targets"][0],
             "chemicals": chemicals,
             "fractions": [0.5, 0.5],
         }
+        requests = data.get_requests_from_candidate(config, candidate)
+        self.assertEqual(len(requests), 1)
+        schemas.Request(**requests[0])  # Validate request with schema
+
+    def test_integration(self):
+        config = {
+            "name": "test_optimiser",
+            "targets": [{
+                "name": "test_target",
+                "quantities": {"test_quantity": "test_quantity"},
+                "method": "test_method",
+                "defaults": {"temperature": 300},
+                "max_queue_size": 1,
+            }]
+        }
+        limitations = [
+            {
+                "quantity": "test_quantity",
+                "method": "test_method",
+                "limitations": [
+                    {
+                        "formulation": [[
+                            {
+                                "chemical": {"SMILES": "test_smiles_1", "InChIKey": "test_inchi_1"},
+                                # "fraction": [{"min": 0.0, "max": 1.0}],
+                                "fraction": [{"min": 0.1, "max": 1.0}, {"min": 0.0, "max": 0.0}],
+                                "fraction_type": "molPerMol"
+                            },
+                            {
+                                "chemical": {"SMILES": "test_smiles_2", "InChIKey": "test_inchi_2"},
+                                # "fraction": [{"min": 0.0, "max": 1.0}],
+                                "fraction": [{"min": 0.1, "max": 1.0}, 0.0],
+                                "fraction_type": "molPerMol"
+                            },
+                        ]],
+                        "temperature": [{"min": 243, "max": 333}]
+                    }
+                ]
+            }
+        ]
+        constraints = data.get_constraints_from_method_limitations(limitations)
+        self.assertIn("test_method", constraints)
+        self.assertEqual(len(constraints["test_method"]), 1)
+        df = None  # TODO: Add dataframe of observed data for data based approach
+        candidates = data.get_best_candidates(config, df, constraints)
+        self.assertIn("test_method", candidates.keys())
+        candidate = candidates["test_method"]
+        self.assertEqual(candidate["target"]["name"], "test_target")
         requests = data.get_requests_from_candidate(config, candidate)
         self.assertEqual(len(requests), 1)
         schemas.Request(**requests[0])  # Validate request with schema
